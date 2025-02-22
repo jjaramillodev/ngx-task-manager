8fa05150882f8674196e308f8904ed6948d6be60
Jack Jaramillo <jjaramillo.dev@gmail.com>
2025-02-22 08:40:39 -0500

    feat(container): agregar propiedad de entrada para el espaciado en el contenedor

--- src/app/shared/components/container/container.component.ts
+++ src/app/shared/components/container/container.component.ts
@@ -1,12 +1,12 @@
-import { Component } from '@angular/core';
+import { Component, input } from '@angular/core';
 
 @Component({
   selector: 'app-container',
   standalone: true,
   imports: [],
   templateUrl: './container.component.html',
-  styleUrl: './container.component.scss'
+  styleUrl: './container.component.scss',
 })
 export class ContainerComponent {
-
+  gap = input(0);
 }

6ad1bd7160daad75d32681e13819e3c2dc982374
Jack Jaramillo <jjaramillo.dev@gmail.com>
2025-02-22 11:17:58 -0500

    feat(tasks): actualizar servicio de tareas para manejar respuestas tipadas

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -1,5 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { inject, Injectable } from '@angular/core';
+import {
+  ResponseTaskI,
+  ResponseTasksI,
+} from '@tasks/interfaces/response-task.interface';
 import { TaskI } from '@tasks/interfaces/task.interface';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.development';
@@ -12,7 +16,11 @@ export class TasksService {
 
   private readonly _apiUrl = environment.apiTasks.url;
 
-  createTask(task: Partial<TaskI>): Observable<TaskI> {
-    return this._http.post<TaskI>(`${this._apiUrl}/tasks`, task);
+  getTasks(): Observable<ResponseTasksI> {
+    return this._http.get<ResponseTasksI>(`${this._apiUrl}/tasks`);
+  }
+
+  createTask(task: Partial<TaskI>): Observable<ResponseTaskI> {
+    return this._http.post<ResponseTaskI>(`${this._apiUrl}/tasks`, task);
   }
 }
